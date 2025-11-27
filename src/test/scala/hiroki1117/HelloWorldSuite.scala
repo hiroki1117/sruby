2ffb6a95bec7f6ae2d6b37415c0d14817c282f12
hiroki1117 <scala1117@gmail.com>
2025-11-27 19:17:37 +0900

    initial commit

---
+++ src/test/scala/hiroki1117/HelloWorldSuite.scala
@@ -0,0 +1,10 @@
+package hiroki1117
+
+import munit.CatsEffectSuite
+
+class HelloWorldSuite extends CatsEffectSuite {
+
+  test("test hello world says hi") {
+    HelloWorld.say().map(it => assertEquals(it, "Hello Cats!"))
+  }
+}
